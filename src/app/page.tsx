82fab2746f811d4a27d83ba897d832882825ea51
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-02-23 19:58:59 -0300

    feat(app): implement post actions, add create post UI and update layout with prisma integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-"use client"
-
-import prisma from "@/lib/prisma";
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server"
 
 export default async function Home() {
-  await prisma
+  const user = await currentUser();
   return (
-    <div className="m-4">
-      <h1>home ṕage content</h1>
+    
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null }
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        WhoToFollow
+      </div>
     </div>
   );
 }
